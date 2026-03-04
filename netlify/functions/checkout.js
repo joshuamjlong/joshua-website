370d631454fd0e9e15e649aeef97812a9617ac41
joshuamjlong <joshuamjlong@gmail.com>
2026-03-04 20:46:53 +0100

    Create checkout.js

---
+++ netlify/functions/checkout.js
@@ -0,0 +1,48 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const { items } = JSON.parse(event.body);
+
+    const line_items = items.map(item => ({
+      price_data: {
+        currency: 'eur',
+        product_data: {
+          name: item.name,
+          description: 'Size: ' + item.size,
+          images: [item.image],
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card', 'apple_pay', 'google_pay', 'p24', 'blik', 'klarna'],
+      line_items,
+      mode: 'payment',
+      success_url: 'https://joshuaatelier.com/#order-success',
+      cancel_url: 'https://joshuaatelier.com/#cart',
+      shipping_address_collection: {
+        allowed_countries: ['PL', 'DE', 'FR', 'IT', 'ES', 'NL', 'BE', 'AT', 'SE', 'DK', 'FI', 'NO', 'PT', 'CZ', 'SK', 'HU', 'RO', 'BG', 'HR', 'SI', 'EE', 'LV', 'LT', 'LU', 'IE', 'GR', 'GB', 'CH'],
+      },
+      automatic_tax: { enabled: false },
+    });
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
